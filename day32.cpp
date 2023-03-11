7785faee0d3decdb10f32f338c2f2816127cc613
Shubham Kumar <shubham49557@gmail.com>
2023-03-11 11:51:18 +0530

    Second Commit

--- day32.cpp
+++ day32.cpp
@@ -1,3 +1,19 @@
+
+/*Day 32 coding Statement : Write a Program to Remove vowels from a string
+
+Description
+
+Get a string as the input from the user and then remove all the vowel letters from the string and give the output.
+
+Input
+
+remove
+
+Output
+
+rmv
+*/
+
 #include<iostream>
 #include<cstring>
 using namespace std;
