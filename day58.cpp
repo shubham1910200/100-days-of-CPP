1a048f5e3928ea22e9c14793ee6310ea7d07c77b
Shubham Kumar <shubham49557@gmail.com>
2023-03-29 19:30:23 +0530

    first commit

---
+++ day58.cpp
@@ -0,0 +1,53 @@
+/*
+Day 58 coding Statement : Bucket Filling
+
+Nejiya has a bucket having a capacity of K liters. It is already filled with X liters of water.
+
+Find the maximum amount of extra water in liters that Nejiya can fill in the bucket without overflowing.
+
+Input Format
+
+The first line will contain T - the number of test cases. Then the test cases follow.
+
+The first and only line of each test case contains two space separated integers K and X - as mentioned in the problem.
+
+Output Format
+
+For each test case, output in a single line, the amount of extra water in liters that Nejiya can fill in the bucket without overflowing.
+
+Sample Input 1
+
+2
+
+5 4
+
+15 6
+
+Sample Output 1
+
+1
+
+9
+*/
+#include<iostream>
+using namespace std;
+void bucket(int n,int x)
+{
+    int rem = n-x;
+    cout<<"Remaining = "<<rem<<endl;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cout<<"Enter the Capacity of bucket water"<<endl;
+        cin>>n;
+        int x;
+        cout<<"Enter the filled water amount"<<endl;
+        cin>>x;
+        bucket(n,x);
+    }
+}
