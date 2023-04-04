ba03f4a7e4ac598e88d33f14fc1f4e8ddf13c80f
Shubham Kumar <shubham49557@gmail.com>
2023-04-04 14:50:23 +0530

    first commit

---
+++ day64.cpp
@@ -0,0 +1,60 @@
+/*
+Day 64 coding Statement : Amit and Feedback
+
+Lots of geeky customers visit our Amit's restaurant everyday. So, when asked to fill the feedback form, these customers represent the feedback using a binary string (i.e a string that contains only characters '0' and '1'.
+
+Now since Amit is not that great in deciphering binary strings, he has decided the following criteria to classify the feedback as Good or Bad :
+
+If the string contains the substring "010" or "101", then the feedback is Good, else it is Bad. Note that, to be Good it is not necessary to have both of them as substring.
+
+So given some binary strings, you need to output whether according to the Amit, the strings are Good or Bad.
+
+Input
+
+The first line contains an integer T denoting the number of feedbacks. Each of the next T lines contains a string composed of only '0' and '1'.
+
+Output
+
+For every test case, print in a single line Good or Bad as per the Amit's method of classification.
+
+Input:
+
+2
+
+11111110
+
+10101010101010
+
+Output:
+
+Bad
+
+Good*/
+
+
+#include<iostream>
+using namespace std;
+
+void feedback(string str)
+{
+    if(str.find("010")!=string::npos || str.find("101")!=string::npos)
+    {
+        cout<<"Good"<<endl;
+    }
+    else
+    {
+        cout<<"BAd"<<endl;
+    }
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+   {
+    string str;
+    cin>>str;
+    feedback(str);
+
+   }
+}
